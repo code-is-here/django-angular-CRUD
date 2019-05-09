47d68d05a7307e1ca8784a7edd18ed9894c699eb
gautamamber <ambergautam1@gmail.com>
2019-05-09 12:19:45 +0530

    component added

---
+++ angularapp/newapp/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from '@angular/core';
+import {Routes,RouterModule, Router} from '@angular/router';
+import { AccountListComponent } from './account-list/account-list.component';
+import { AccountCreateComponent } from './account-create/account-create.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactCreateComponent } from './contact-create/contact-create.component';
+import { ActivityListComponent } from './activity-list/activity-list.component';
+import { ActivityCreateComponent } from './activity-create/activity-create.component';
+
+const routes : Routes=[
+    { path: 'accounts', component: AccountListComponent},
+];
+
+@NgModule({
+    imports:[RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule{ }
