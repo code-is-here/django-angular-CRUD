c46236772fcdac827f5f212aef166429e3c4be3b
gautamamber <ambergautam1@gmail.com>
2019-05-09 10:51:27 +0530

    models added

--- new/admin.py
+++ new/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Account, Activity, Contact, ContactSource, ContactStatus, ActivityStatus
 # Register your models here.
+admin.site.register(Activity)
+admin.site.register(Account)
+admin.site.register(ActivityStatus)
+admin.site.register(ContactSource)
+admin.site.register(ContactStatus)
+admin.site.register(Contact)
--- new/models.py
+++ new/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+INDCHOICES = (
+    ('FINANCE', 'FINANCE'),
+    ('HEALTHCARE', 'HEALTHCARE'),
+    ('INSURANCE', 'INSURANCE'),
+    
+)
+
+class Account(models.Model):
+    name = models.CharField(max_length=64)
+    email = models.EmailField(blank = True, null = True)
+    phone = models.CharField(max_length=20, blank = True, null = True)
+    industry = models.CharField(max_length=255, choices=INDCHOICES, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
+    createdBy = models.ForeignKey(User, related_name='account_created_by', on_delete=models.CASCADE)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    isActive = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
+
+class ContactSource(models.Model):
+    status = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.status
+
+class ContactStatus(models.Model):
+    status = models.CharField( max_length=20)
+
+    def __str__(self):
+        return self.status
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=255, blank = True, null = True)
+    last_name = models.CharField( max_length=255, blank = True, null = True)
+    account = models.ForeignKey(Account, related_name='lead_account_contacts', on_delete=models.CASCADE, blank=True, null=True)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20, blank = True, null = True)
+    createdBy = models.ForeignKey(User, related_name='contact_created_by', on_delete=models.CASCADE)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    isActive = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.first_name
+
+class ActivityStatus(models.Model):
+    status = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.status
+
+class Activity(models.Model):
+    description = models.TextField(blank=True, null=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.description
